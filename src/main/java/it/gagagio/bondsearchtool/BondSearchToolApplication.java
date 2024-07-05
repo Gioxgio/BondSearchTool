9e0eb6aacd6385e0010471bcd2109d8856a18cf5
Gioxgio <fantastici02@hotmail.it>
2024-07-05 13:58:48 +0200

    First commit

---
+++ src/main/java/it/gagagio/bondsearchtool/BondSearchToolApplication.java
@@ -0,0 +1,13 @@
+package it.gagagio.bondsearchtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondSearchToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondSearchToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gagagio/bondsearchtool/BondSearchToolApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gagagio.bondsearchtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondSearchToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
