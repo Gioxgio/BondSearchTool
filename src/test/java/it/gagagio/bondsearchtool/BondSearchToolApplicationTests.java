e93c3a8fe2a01c0b8c7786b447d5d8921cbe3966
Gioxgio <fantastici02@hotmail.it>
2024-07-06 15:11:38 +0200

    Update project configuration

--- src/test/java/it/gagagio/bondsearchtool/BondSearchToolApplicationTests.java
+++ src/test/java/it/gagagio/bondsearchtool/BondSearchToolApplicationTests.java
@@ -1,13 +1,8 @@
 package it.gagagio.bondsearchtool;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BondSearchToolApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
