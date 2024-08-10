d91ced4c93e9caa34a53f76ba48fb0e8568cf895
Gioxgio <fantastici02@hotmail.it>
2024-08-10 16:28:58 +0200

    Add logic to add new bonds and remove outdated ones

--- src/main/java/it/gagagio/bondsearchtool/data/repository/BondRepository.java
+++ src/main/java/it/gagagio/bondsearchtool/data/repository/BondRepository.java
@@ -4,13 +4,21 @@ import it.gagagio.bondsearchtool.data.entity.BondEntity;
 import it.gagagio.bondsearchtool.model.BondType;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public interface BondRepository extends JpaRepository<BondEntity, String> {
+
+    @Query("SELECT DISTINCT(b.isin) FROM bond b")
+    Set<String> findAllIsins();
 
     List<BondEntity> findByYieldToMaturityIsNullAndMarketIn(final Collection<String> markets, final Limit limit);
 
     List<BondEntity> findByTypeIsNullOrTypeEquals(final BondType type, final Limit limit);
+
+    int removeByIsinIn(final Collection<String> isins);
 }
+
--- src/main/java/it/gagagio/bondsearchtool/service/ScraperService.java
+++ src/main/java/it/gagagio/bondsearchtool/service/ScraperService.java
@@ -1,8 +1,10 @@
 package it.gagagio.bondsearchtool.service;
 
+import it.gagagio.bondsearchtool.data.entity.BondEntity;
 import it.gagagio.bondsearchtool.data.entity.BondEntityMapper;
 import it.gagagio.bondsearchtool.data.repository.BondRepository;
 import it.gagagio.bondsearchtool.euronext.Euronext;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Component;
@@ -15,11 +17,20 @@ public class ScraperService {
     private final BondEntityMapper bondEntityMapper;
     private final BondRepository bondRepository;
 
+    @Transactional
     public int refresh() {
 
         val bonds = euronext.refresh();
         val bondEntities = bonds.stream().map(bondEntityMapper::getBondEntityFromBond).toList();
 
-        return bondRepository.saveAllAndFlush(bondEntities).size();
+        val newIsins = bondEntities.stream().map(BondEntity::getIsin).toList();
+        val existingIsins = bondRepository.findAllIsins();
+
+        val newBondEntities = bondEntities.stream().filter(b -> !existingIsins.contains(b.getIsin())).toList();
+        val outdatedBondEntities = existingIsins.stream().filter(i -> !newIsins.contains(i)).toList();
+
+        bondRepository.removeByIsinIn(outdatedBondEntities);
+
+        return bondRepository.saveAllAndFlush(newBondEntities).size();
     }
 }
